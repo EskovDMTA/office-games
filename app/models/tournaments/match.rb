e4988a016c2340f328dc545eec91af696431e1ce
eskovdmt <DmtEskov@gmail.com>
2024-05-11 00:20:44 +0300

    feat: update: tournaments models

--- app/models/tournaments/match.rb
+++ app/models/tournaments/match.rb
@@ -1,6 +1,15 @@
 class Tournaments::Match < ApplicationRecord
-  belongs_to :tournaments_tournament, class_name: 'Tournaments::Tournament'
-  has_one :tournaments_match_result, class_name: 'Tournaments::MatchResult'
+  belongs_to :tournament, class_name: 'Tournaments::Tournament'
+  has_one :match_result, class_name: 'Tournaments::MatchResult', foreign_key: :match_id
   belongs_to :participant1, polymorphic: true
   belongs_to :participant2, polymorphic: true
+
+  validate :match_date_within_tournament_duration
+
+  def match_date_within_tournament_duration
+    unless tournaments_tournament.start_date <= match_date && match_date <= tournaments_tournament.end_date
+      errors.add(:match_date, "дата матча не может выходить за временной промежуток проведения турнира")
+    end
+  end
+
 end
--- app/models/tournaments/tournament.rb
+++ app/models/tournaments/tournament.rb
@@ -1,4 +1,5 @@
 class Tournaments::Tournament < ApplicationRecord
   has_many :tournaments_tournament_results, class_name: 'Tournaments::TournamentResult'
+  has_many :tournaments_matches, :class_name => 'Tournaments::Match'
   belongs_to :game
 end
