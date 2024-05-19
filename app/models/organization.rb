7a44d57990e5e046f5f32857824f99552fe1352c
eskovdmt <DmtEskov@gmail.com>
2024-05-19 22:28:52 +0300

    feat: add some validations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,4 +4,16 @@ class Organization < ApplicationRecord
   has_many :team_users
   has_many :teams
   has_many :tournaments, class_name: 'Tournaments::Tournament'
+
+  validates :name, :description, :address, :email, :telegram, :owner_id, presence: true
+
+  validates :name, length: { minimum: 3, maximum: 50 }, uniqueness: true
+  validates :telegram, length: { minimum: 3, maximum: 20 }, uniqueness: { case_sensitive: false }
+  validates :description, length: { minimum: 15, maximum: 250 }
+  validates :address, length: { minimum: 3, maximum: 100 }
+
+  validates :email, presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            length: { minimum: 3, maximum: 50 }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -12,4 +12,9 @@ class Team < ApplicationRecord
   has_many :tournaments_match_results, class_name: 'Tournaments::MatchResult', as: :participant2
 
   has_many :tournaments, class_name: 'Tournaments::Tournament', as: :winner
+
+  validates :name, :description, :organization_id, :captain_id, presence: true
+
+  validates :name, length: {minimum: 3, maximum: 50}
+  validates :description, length: {minimum: 15, maximum: 250}
 end
